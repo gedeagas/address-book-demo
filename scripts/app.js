5fa8335846ac7f973e0a18b16c62dfff224b26db
Tiffany White <tiffanywhitedev412@gmail.com>
2016-05-05 00:49:20 -0400

    Adds more javascript, travis CI, and some other HTNL to the addContacts file

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,19 @@
-//
+// Empty arrays to push contacts onto.
+var names = [];
+var emails = [];
+var telenums = [];
 
-var contacts = [];
+// Need to write a function to gather first names and last names through input
+
+function getInput() {
+  var userName = document.getElementById('inputName');
+  var userEmail = document.getElementById('inputEmail');
+  var userTeleNum = document.getElementById('inputPhoneNumber');
+}
+
+$('#form').on('submit', function () {
+    //your awesome code here
+    alert('Got it!');
+})
+// Need to write a function gather phone numbers through input
+// Need to write a function to gather emails through input.
